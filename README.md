ac19a947492d8537264422390e8d073f4510e5fb
Zheng Jia <jia@saleswhale.com>
2020-01-06 16:46:26 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Schooling
+
+A simply Python wrapper around Redis streams.
+
+## Installation
+
+### From Git
+Install via
+```bash
+pip install git+https://github.com/saleswhale/schooling-py.git
+```
+
+or
+
+```bash
+pip install git+ssh://git@github.com/saleswhale/schooling-py.git
+```
+
+### From source
+Navigate into the root directory `schooling-py` and run
+```bash
+python setup.py install
+```
---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open('README.md', 'r') as readme:
+    long_description = readme.read()
+
+with open('requirements.txt', 'r') as reqs:
+    requirements = [req.strip() for req in reqs.readlines()]
+
+setuptools.setup(
+    name='schooling',
+    version='0.1',
+    author='Saleswhale',
+    author_email='jia@saleswhale.com',
+    description='A tiny Python wrapper for Redis streams.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=setuptools.find_packages(exclude=['tests']),
+    install_requires=requirements,
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+    python_requires='>=3.6'
+)
